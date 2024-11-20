e7a96967efc2368f3c43285109509549a43dd326
changsu705 <2021137031@g.shingu.ac.kr>
2024-11-21 02:11:41 +0900

    잘안됨
    잘안됨

--- Assets/Scropts/MoveTest.cs
+++ Assets/Scropts/MoveTest.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 
 public class MoveTest : MonoBehaviour
 {
-    public Vector3 MoveOffSet;
+    public Vector3 MoveOffset;
+    public Vector3 SpeedOffset;
+    public Vector3 JumpOffset;
 
     // Start is called before the first frame update
     void Start()
